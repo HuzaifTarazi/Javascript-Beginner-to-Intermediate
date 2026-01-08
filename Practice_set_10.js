ddf43275983c07a096cf47e93cf62ac8d1c2551e
Huzaif Tarazi <huzaiftarazi14@gmail.com>
2026-01-08 22:41:37 +0500

    question 1 practice set 10

--- Practice_set_10.js
+++ Practice_set_10.js
@@ -18,3 +18,42 @@ round, floor, ceil, random, max, min
 pow, sqrt, abs, trunc
 .checked, Trinary Function , Switch Case
 --------------------------------------------*/
+
+const checkDay = document.getElementById("checkDay");
+function whichDay() {
+  const dayInput = document.getElementById("dayInput");
+  const printDay = document.getElementById("printDay");
+  let chngType = Number(dayInput.value);
+
+  switch (chngType) {
+    case 1:
+      printDay.textContent = `It's Monday Today`;
+      dayInput.value = "";
+      break;
+    case 2:
+      printDay.textContent = `It's Tuesday Today`;
+      dayInput.value = "";
+      break;
+    case 3:
+      printDay.textContent = `It's Wednesday Today`;
+      dayInput.value = "";
+      break;
+    case 4:
+      printDay.textContent = `It's Thursday Today`;
+      dayInput.value = "";
+      break;
+    case 5:
+      printDay.textContent = `It's Friday Today`;
+      dayInput.value = "";
+      break;
+    case 6:
+      printDay.textContent = `It's Saturday Today`;
+      dayInput.value = "";
+      break;
+    case 7:
+      printDay.textContent = `It's Sunday Today`;
+      dayInput.value = "";
+      break;
+  }
+}
+checkDay.onclick = whichDay;
