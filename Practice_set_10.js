af826ec7db5338d838414c6232a735e3db87549a
Huzaif Tarazi <huzaiftarazi14@gmail.com>
2026-01-08 22:05:02 +0500

    Practice set 10 add html update

---
+++ Practice_set_10.js
@@ -0,0 +1,20 @@
+/* -----------------------------------------
+               << Practice Set 10 >>
+
+let, const
++ - * / %
+> < >= <= === !==
+&& || !
+if, else if, else, nested if else
+Template literals ${}
+getElementById().value
+getElementById().textContent
+getElementById().onclick
+Number(), parseFloat()
+console.log
+
+Math:
+round, floor, ceil, random, max, min
+pow, sqrt, abs, trunc
+.checked, Trinary Function , Switch Case
+--------------------------------------------*/
