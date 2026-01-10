d9fb970e240950c64cb0eae68ccedada5421d761
Huzaif Tarazi <huzaiftarazi14@gmail.com>
2026-01-10 21:04:34 +0500

    practice set 11

---
+++ Practice_set_11.js
@@ -0,0 +1,31 @@
+/* -----------------------------------------
+               << Practice Set 10 >>
+
+let, const
++ - * / %
+> < >= <= === !==
+&& || !
+if, else if, else, nested if else
+Template literals ${}
+getElementById().value
+getElementById().textContent
+getElementById().onclick
+Number(), parseFloat()
+console.log
+
+Math:
+round, floor, ceil, random, max, min
+pow, sqrt, abs, trunc
+
+.checked, 
+Trinary Function, 
+Switch Case.
+
+String Operation:
+length, toUpperCase(), toLowerCase(), trim(), 
+includes(), startsWith(), endsWith(), indexOf(), 
+slice(), subString(), replace(), replaceAll, split(),
+charAt(),
+
+--------------------------------------------*/
+
