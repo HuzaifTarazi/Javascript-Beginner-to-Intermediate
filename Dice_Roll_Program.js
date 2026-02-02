34764ae29c82a35811cc2ac9ada2249002076ed4
Huzaif Tarazi <huzaiftarazi14@gmail.com>
2026-02-02 22:27:29 +0500

    Dice Roll Game Program Add

---
+++ Dice_Roll_Program.js
@@ -0,0 +1,31 @@
+// Dice Roll Program
+
+
+
+function DiceRollProgram(diceAttempts) {
+
+    let tempArr = [];
+    let imageArr = [];
+    if (!diceAttempts || isNaN(diceAttempts)) {
+        alert("Please Insert Number Correctly.!");
+    }
+
+    for (i = 1; i <= diceAttempts; i++) {
+        const randomNumber = Math.floor(Math.random() * 6) + 1;
+        tempArr.push(randomNumber);
+        imageArr.push(`<img src="./dice_images/${randomNumber}.jpg" alt="Dice ${randomNumber}">`)
+
+    }
+    const diceResult = document.getElementById("diceResult");
+    const diceImage = document.getElementById("diceImage");
+    diceResult.textContent = `Dice Results ${tempArr.join(", ")}`
+    diceImage.innerHTML = imageArr.join('-');
+
+}
+
+
+const rollBtn = document.getElementById("rollBtn");
+rollBtn.onclick = function () {
+    const diceInput = document.getElementById("diceInput").value;
+    DiceRollProgram(diceInput);
+}
