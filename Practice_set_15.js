6495f53e6197356d020b23ad49bcaa541875bb32
Huzaif Tarazi <huzaiftarazi14@gmail.com>
2026-02-14 18:14:53 +0500

    practice set 15 updated js

---
+++ Practice_set_15.js
@@ -0,0 +1,52 @@
+/* -----------------------------------------
+               << Practice Set 13 >>
+
+let, const
++ - * / %
+> < >= <= === !==
+&& || !
+if, else if, else, nested if else
+Template literals ${}
+getElementById().value
+getElementById().textContent
+getElementById().onclick
+Number(), parseFloat()
+console.log
+
+Math:
+round, floor, ceil, random, max, min
+pow, sqrt, abs, trunc
+
+.checked, 
+Trinary Function, 
+Switch Case.
+
+String Operation:
+length, toUpperCase(), toLowerCase(), trim(), 
+includes(), startsWith(), endsWith(), indexOf(), 
+slice(), subString(), replace(), replaceAll, split(),
+charAt(),
+
+Method Chaining , While Loop, For Loop
+Functions, Function Parameters,Function Arguments, Return Statement
+
+Variable Scopes (Local , Global)
+
+Array, Array Methods:
+push(), pop(), shift(), unshift(), splice(), slice(),
+indexOf(), includes(), join(), toString(), 
+concat(), length, reverse(), sort(), 
+
+
+Spread Operator, Spread Operator(...), Rest Parameter(...)
+
+forEach(), map(), filter(), reduce()
+-------------------------------------------- */
+
+/* 1:
+   
+  
+*/
+
+
+/*<-------------------------->*/
