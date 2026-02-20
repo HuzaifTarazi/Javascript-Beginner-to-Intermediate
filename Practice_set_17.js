7e15abd177b65be3946d8b875e6b0c41276b9282
Huzaif Tarazi <huzaiftarazi14@gmail.com>
2026-02-20 16:38:19 +0500

    practice set 17 question 1

--- Practice_set_17.js
+++ Practice_set_17.js
@@ -1,67 +1,99 @@
-// class person {
-//     constructor(name, age, fatherName, motherName) {
-//         this.name = name;
-//         this.age = age;
-//         this.fatherName = fatherName;
-//         this.motherName = motherName;
-//     }
+/* -----------------------------------------
+               << Practice Set 16 >>
 
-//     greet() {
-//         console.log(`Hello, My name is ${this.name} and I am ${this.age} years old.`)
-//     }
-// }
+let, const
++ - * / %
+> < >= <= === !==
+&& || !
+if, else if, else, nested if else
+Template literals ${}
+getElementById().value
+getElementById().textContent
+getElementById().onclick
+Number(), parseFloat()
+console.log
+
+Math:
+round, floor, ceil, random, max, min
+pow, sqrt, abs, trunc
+
+.checked,
+Trinary Function,
+Switch Case.
+
+String Operation:
+length, toUpperCase(), toLowerCase(), trim(),
+includes(), startsWith(), endsWith(), indexOf(),
+slice(), subString(), replace(), replaceAll, split(),
+charAt(),
+
+Method Chaining , While Loop, For Loop
+Functions, Function Parameters,Function Arguments, Return Statement
+
+Variable Scopes (Local , Global)
+
+Array, Array Methods:
+push(), pop(), shift(), unshift(), splice(), slice(),
+indexOf(), includes(), join(), toString(),
+concat(), length, reverse(), sort(),
+
+Spread Operator, Spread Operator(...), Rest Parameter(...), Callback Function
+
+Callbacks, forEach(), map(), filter(), reduce(), findIndex()
+
+Function Expression , Arrow Function, Objects, This Method
+
+Class, Constructor, Object Creation Using Class, Static Keyword
+
+-------------------------------------------- */
 
 
-// let saveData = new person("Huzaif", 25, "Rauf", "Nazli Rauf");
-// let newData = new person("Zukhruf", 23, "Muhammad Rauf", "Nazli Rauf")
+/* 1:
+    Practice Set 1 — Student Result System
+    Create a Student class:
+    Properties:
+        name
+        marks
+    Methods:
+        getGrade()
+        ≥ 80 → "A"
+        ≥ 60 → "B"
+        else → "C"
+        getResult()
+        ≥ 50 → "Pass"
+        else → "Fail"
+        getDetails()
+        Return full string
+// */
 
-// console.log(newData)
-
-
-// Create a class Student
-
-// Add properties:
-
-// name
-
-// marks
-
-// Add methods:
-
-// getDetails() → prints name and marks
-
-// getGrade() →
-
-// marks >= 80 → "A"
-
-// marks >= 60 → "B"
-
-// else → "C"
-
-
-class student {
+class StudentResultSystem {
+    // Properties
     constructor(name, marks) {
         this.name = name;
         this.marks = marks;
     }
-
-    getDetails() {
-        return `The Student Name is ${this.name} and Total Marks Obtained are ${this.marks}`
+    // Methods
+    getGrade(){
+        if(this.marks >= 80){
+            return `You Have Got Grade A`;
+        } else if(this.marks >= 60){
+            return `You Have Got Grade B`;
+        } else {
+            return `You Have Got Grade C`;
+        }
     }
 
-    getGrade() {
-        if (this.marks >= 80) {
-            return "You Got A Grade";
-        } else if (this.marks >= 60) {
-            return "You Got B Grade";
-        } else {
-            return "You Got C Grade"
-        }
+    getResult(){
+        return this.marks >= 50 ? "You Have Pass The Exams" : "You Have Failed The Exams";
+    }
+
+    getDetails(){
+        return `Student Name: ${this.name} \n Student Marks: ${this.marks} \n ${this.getGrade()} \n ${this.getResult()}`
     }
 }
 
-let Student1 = new student("Huzaif Tarazi", 30)
+let student1 = new StudentResultSystem("Huzaif Tarazi", 70);
 
+console.log(student1.getDetails())
 
-console.log(Student1.getDetails());
-console.log(Student1.getGrade());
+/*<-------------------------->*/
