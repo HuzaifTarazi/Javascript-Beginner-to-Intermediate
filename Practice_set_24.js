e598b54b469fa2399f0f7b036016ccdf5baf2f6f
Huzaif Tarazi <huzaiftarazi14@gmail.com>
2026-03-04 18:02:00 +0500

    practice set 24 digital clock

---
+++ Practice_set_24.js
@@ -0,0 +1,27 @@
+// Digital Clock
+
+
+function digitalClock() {
+
+    const time = new  Intl.DateTimeFormat("en-US", {
+        hour: `2-digit`,
+        minute: `2-digit`,
+        second: `2-digit`,
+        hour12: true,
+    })
+    const newTime = time.formatToParts(new Date())
+
+    const [hour, colon1 , minute, colon2 ,second, colon3 , hour12] = newTime 
+
+    const setHour = document.getElementById("hours");
+    setHour.textContent = hour.value
+    const setMinute = document.getElementById("minutes");
+    setMinute.textContent = minute.value;
+    const setSeconds = document.getElementById("seconds");
+    setSeconds.textContent = second.value
+    const dayPeriod = document.getElementById("period");
+    dayPeriod.textContent = hour12.value
+}
+
+setInterval(digitalClock, 1000)
+
